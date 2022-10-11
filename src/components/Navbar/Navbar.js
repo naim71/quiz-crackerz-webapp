1a99d5679a6d30605e9669be42516cbc2f1f0ae8
naim71 <naimkhan1314@gmail.com>
2022-10-12 00:41:12 +0600

    navbar updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,15 +13,15 @@ const Navbar = () => {
     
     return (
         <div>
-            <div onClick={() => setOpen(!open)} className="h-8 w-8 text-sky-800">
+            <div onClick={() => setOpen(!open)} className="h-8 w-8 text-sky-800 md:hidden mt-2 ml-2">
             {
                 open ?  <XCircleIcon/>  : <Bars3Icon/>
             }
             </div>
            
-            <nav className='flex justify-between items-center mx-20 px-10 my-10'>
+            <nav className='md:flex justify-between items-center mx-20 px-10 my-10'>
                 <h1 className='text-sky-800 font-bold text-3xl'><a href='/'>Quizology</a></h1>
-                <ul className='flex space-x-12'>
+                <ul className={`md:flex md:space-x-12 absolute  md:static ${open ? 'bg-white left-0 top-10 w-full' : 'top-[-200px]'}`}>
                     {
                         routes.map(route => <Link key={route.id} route={route}></Link>)
                     }
