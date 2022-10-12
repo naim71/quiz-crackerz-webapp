24f820eea4a99e057835342577b53df598a1511f
naim71 <naimkhan1314@gmail.com>
2022-10-12 16:56:41 +0600

    question section added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,11 +4,11 @@ import QuizQuestions from '../QuizQuestions/QuizQuestions';
 
 const Quiz = () => {
     const quiz = useLoaderData();
-    console.log(quiz.data.name);
     const quizQstn = quiz.data.questions;
     return (
         <div className='bg-white mx-20 rounded-xl'>
-            <h1 className=''>Quiz of {quiz.data.name}</h1>
+            <h1 className='text-sky-800 text-2xl font-bold p-5'>Quiz of {quiz.data.name}</h1>
+            <div  className='grid grid-cols-1 gap-6 mx-auto'>
             {
                 quizQstn.map(qstn => <QuizQuestions
                     key={qstn.id}
@@ -16,6 +16,8 @@ const Quiz = () => {
                 >
                 </QuizQuestions>)
             }
+            </div>
+            
         </div>
     );
 };
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const QuizQuestions = ({qstn}) => {
-    // console.log(qstn);
-    const {id, qustion, correctAnswer, options} = qstn;
+    console.log(qstn);
+    const {id, question, correctAnswer, options} = qstn;
     return (
         <div>
-            <h1>Quiz of {}</h1>
+          
+        <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
+         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{question}</h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+
         </div>
     );
 };
