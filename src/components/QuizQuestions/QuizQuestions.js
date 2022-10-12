07f9faaac4247e0f174a67cc24b10d813fb4588b
naim71 <naimkhan1314@gmail.com>
2022-10-12 20:07:46 +0600

    eye shows correct answer

--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { EyeIcon } from '@heroicons/react/24/solid'
+import React, { useState } from 'react';
+import "./QuizQuestions.css";
+import { EyeIcon, XMarkIcon, CheckCircleIcon } from '@heroicons/react/24/solid'
 
 
 const QuizQuestions = ({qstn}) => {
     console.log(qstn);
-    const {id, question, correctAnswer, options} = qstn;
+    const {question, correctAnswer, options} = qstn;
+
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+    setModal(!modal);
+    };
+
+     if(modal) {
+    document.body.classList.add('active-modal')
+     } else {
+    document.body.classList.remove('active-modal')
+    }
+
     return (
         <div>
           
         <div className="block p-6 md:w-8/12 md:h-11/12 bg-white rounded-lg shadow-xl dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mx-auto">
          <div className='relative'>
-            <button><EyeIcon className="h-6 w-6 text-violet-600 absolute right-0 top-0"/></button>
+            <button onClick={toggleModal}><EyeIcon className="h-6 w-6 text-violet-600 absolute right-0 top-0"/>
+            {modal && (
+                <div className="modal relative">
+                  <div onClick={toggleModal} className="overlay"></div>
+                    <div className="modal-content">
+                      <CheckCircleIcon className='mx-auto h-10 w-10 text-green-600'/> 
+                      <h2 className='text-lg mb-5 font-semibold'>Correct Answer</h2>
+                     <p className='mb-5 text-green-600 text-lg font-semibold border border-green-500 rounded-lg p-4'>{correctAnswer}</p>
+            <button className="close-modal" onClick={toggleModal}><XMarkIcon className='absolute right-0 top-0 h-6 w-6 text-red-600'/></button>
+          </div>
+        </div>
+      )}
+            </button>
+            
+            
+
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white px-11">{question.slice(3,-4)}</h5>
 
          </div>
