dcc60eccab76b84c532d7637b29bda3da07ede1b
naim71 <naimkhan1314@gmail.com>
2022-10-12 16:42:49 +0600

    quiz section

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import QuizQuestions from '../QuizQuestions/QuizQuestions';
 
 const Quiz = () => {
     const quiz = useLoaderData();
-    console.log(quiz.data.questions);
+    console.log(quiz.data.name);
+    const quizQstn = quiz.data.questions;
     return (
-        <div>
-            <h1>hi</h1>
+        <div className='bg-white mx-20 rounded-xl'>
+            <h1 className=''>Quiz of {quiz.data.name}</h1>
+            {
+                quizQstn.map(qstn => <QuizQuestions
+                    key={qstn.id}
+                    qstn={qstn} 
+                >
+                </QuizQuestions>)
+            }
         </div>
     );
 };
---
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuizQuestions = ({qstn}) => {
+    // console.log(qstn);
+    const {id, qustion, correctAnswer, options} = qstn;
+    return (
+        <div>
+            <h1>Quiz of {}</h1>
+        </div>
+    );
+};
+
+export default QuizQuestions;
